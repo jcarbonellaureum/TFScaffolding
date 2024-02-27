ed26773a00fc7ef97a163991bf267552216f940c
Jose Carbonell <jcarbonell@aureum.us>
2024-02-27 13:24:38 -0300

    adding random to dynamodb table name

--- backend.tf
+++ backend.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_b
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name              = "terraform-state-locking"
+  name              = "terraform-state-locking-${random_string.resource_id.result}"
   billing_mode      = "PAY_PER_REQUEST"
   hash_key          = "LockID"
   attribute {
